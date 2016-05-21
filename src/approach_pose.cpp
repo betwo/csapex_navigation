183f699924ccd11043830d21f7b8b39519a6f276
buck <sebastian.buck@uni-tuebingen.de>
2016-05-22 01:02:12 +0200

    Added a node that drives a robot to a pose using path planning; Added a node that drives a robot to a pose reactively

---
+++ src/approach_pose.cpp
@@ -0,0 +1,224 @@
+/// COMPONENT
+#include <csapex/model/node.h>
+#include <csapex/msg/io.h>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/msg/io.h>
+#include <csapex_ros/yaml_io.hpp>
+#include <csapex/msg/generic_pointer_message.hpp>
+#include <csapex_ros/ros_message_conversion.h>
+#include <csapex/model/token.h>
+#include <csapex/signal/event.h>
+#include <csapex_transform/transform_message.h>
+#include <csapex/model/node_handle.h>
+
+/// SYSTEM
+#include <tf/tf.h>
+#include <geometry_msgs/Twist.h>
+#include <visualization_msgs/MarkerArray.h>
+
+using namespace csapex::connection_types;
+
+namespace csapex
+{
+
+class ApproachPose : public Node
+{
+public:
+    ApproachPose()
+    {
+    }
+
+    void setup(csapex::NodeModifier& modifier) override
+    {
+        in_pose_ = modifier.addInput<TransformMessage>("robot pose");
+
+        out_twist_ = modifier.addOutput<geometry_msgs::Twist>("command");
+        out_marker_ = modifier.addOutput<visualization_msgs::MarkerArray>("markers");
+
+        event_at_goal_ = modifier.addEvent("at goal");
+        event_error_ = modifier.addEvent("error");
+
+        modifier.addTypedSlot<TransformMessage>("odom pose", [this](const TokenPtr& token) {
+            goal_ = std::dynamic_pointer_cast<TransformMessage const>(token->getTokenData());
+            apex_assert(goal_);
+        });
+
+
+        node_handle_->setProcessingEnabled(false);
+    }
+
+    void activation()
+    {
+        last_error_pos_ = std::numeric_limits<double>::infinity();
+        node_handle_->setProcessingEnabled(true);
+    }
+
+    void deactivation()
+    {
+        node_handle_->setProcessingEnabled(false);
+    }
+
+    void setupParameters(csapex::Parameterizable& parameters) override
+    {
+        parameters.addParameter(param::ParameterFactory::declareRange("distance", -2.0, 2.0, 0.0, 0.01), distance_);
+    }
+
+    void process()
+    {
+        if(!goal_) {
+            awarn << "no goal set!" << std::endl;
+            return;
+        }
+
+        pose_ = msg::getMessage<TransformMessage>(in_pose_);
+
+        tf::Pose pose_object_odom = goal_->value;
+
+        tf::Transform odom_to_base_link = pose_->value;
+        tf::Pose pose_object_base_link = odom_to_base_link * pose_object_odom;
+
+        // offset_moves
+        double offset_distance = distance_;
+        double min_offset_distance = -1.5;
+
+        double min_distance_to_robot = 0.4;
+
+
+        tf::Transform goal_offset(tf::createIdentityQuaternion(), tf::Vector3(distance_, 0, 0));
+        tf::Pose goal_error = pose_object_base_link * goal_offset;
+
+        tf::Pose error = goal_error;
+        while(offset_distance > min_offset_distance) {
+            tf::Transform offset(tf::createIdentityQuaternion(), tf::Vector3(offset_distance, 0, 0));
+            error = pose_object_base_link * offset;
+            if(error.getOrigin().length() < min_distance_to_robot) {
+                break;
+            }
+
+            offset_distance -= 0.05;
+        }
+
+        error.setRotation(tf::createQuaternionFromYaw(std::atan2(error.getOrigin().y(), error.getOrigin().x())));
+
+        driveToPose(error, goal_error);
+
+        if(msg::isConnected(out_marker_)) {
+            visualization_msgs::Marker orientation_marker = makeMarker(0.0, 1.0, 0.0, "error", 1);
+
+            orientation_marker.header.frame_id = "/base_link";
+            orientation_marker.pose.position.x = error.getOrigin().x();
+            orientation_marker.pose.position.y = error.getOrigin().y();
+            tf::quaternionTFToMsg(error.getRotation(), orientation_marker.pose.orientation);
+            orientation_marker.type = visualization_msgs::Marker::ARROW;
+            orientation_marker.scale.x = 0.5;
+            orientation_marker.scale.y = 0.075;
+            orientation_marker.scale.z = 0.075;
+
+            std::shared_ptr<visualization_msgs::MarkerArray> markers = std::make_shared<visualization_msgs::MarkerArray>();
+            markers->markers.push_back(orientation_marker);
+
+            visualization_msgs::Marker error_marker = makeMarker(1.0, 0.0, 0.0, "error", 0);
+            error_marker.header.frame_id = "/base_link";
+            geometry_msgs::Point start, end;
+            end.x = error.getOrigin().x();
+            end.y = error.getOrigin().y();
+            error_marker.points.push_back(start);
+            error_marker.points.push_back(end);
+            error_marker.type = visualization_msgs::Marker::ARROW;
+            error_marker.scale.x = 0.025;
+            error_marker.scale.y = 0.075;
+            error_marker.scale.z = 0;
+
+            markers->markers.push_back(error_marker);
+
+            msg::publish(out_marker_, markers);
+        }
+    }
+
+
+    void driveToPose(const tf::Pose &error, const tf::Pose &goal_error)
+    {
+        geometry_msgs::Twist twist;
+        //    double error_yaw = tf::getYaw(error.getRotation());
+
+        double e = std::hypot(error.getOrigin().x(), error.getOrigin().y());
+        double ge = std::hypot(goal_error.getOrigin().x(), goal_error.getOrigin().y());
+
+        bool pos_good = ge < 0.01 || (ge < 0.07 && ge > last_error_pos_);
+
+        double error_yaw = std::atan2(error.getOrigin().y(), error.getOrigin().x());
+
+        if(std::abs(ge) < 0.05) {
+            error_yaw = 0;
+        }
+
+        if(pos_good) {
+            event_at_goal_->trigger();
+
+        } else {
+            double ex = error.getOrigin().x() ;
+            double ey = error.getOrigin().y();
+            twist.linear.x = ex * 0.6;
+            double speed = std::abs(twist.linear.x);
+
+            double max_speed = 0.3;
+            double min_speed = 0.15;
+
+            if(speed > max_speed) {
+                twist.linear.x *= max_speed / speed;
+                twist.linear.y *= max_speed / speed;
+            } else if(speed < min_speed) {
+                twist.linear.x *= min_speed / speed;
+                twist.linear.y *= min_speed / speed;
+            }
+            twist.angular.z =  error_yaw * 1.25;
+        }
+
+        last_error_pos_ = ge;
+
+        msg::publish(out_twist_, std::make_shared<geometry_msgs::Twist>(twist));
+    }
+
+    static visualization_msgs::Marker makeMarker(float r, float g, float b, const std::string& ns, int id)
+    {
+        visualization_msgs::Marker marker;
+        marker.ns = ns;
+        marker.header.frame_id = "/map";
+        marker.header.stamp = ros::Time();
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.id = id;
+        marker.color.r = r;
+        marker.color.g = g;
+        marker.color.b = b;
+        marker.color.a = 1.0;
+        marker.scale.x = 1.0;
+        marker.scale.y = 1.0;
+        marker.scale.z = 1.0;
+        marker.type = visualization_msgs::Marker::CUBE;
+
+        return marker;
+    }
+
+
+private:
+    Input* in_pose_;
+    Output* out_twist_;
+    Output* out_marker_;
+
+    Event* event_at_goal_;
+    Event* event_error_;
+
+    TransformMessage::ConstPtr pose_;
+    TransformMessage::ConstPtr goal_;
+
+    double distance_;
+    double last_error_pos_;
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::ApproachPose, csapex::Node)
+
+
---
+++ src/goto.cpp
@@ -0,0 +1,129 @@
+/// COMPONENT
+#include <csapex/model/node.h>
+#include <csapex/msg/io.h>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/msg/io.h>
+#include <csapex_ros/yaml_io.hpp>
+#include <csapex/msg/generic_pointer_message.hpp>
+#include <csapex_ros/ros_message_conversion.h>
+#include <csapex/model/token.h>
+#include <csapex/signal/event.h>
+#include <csapex_transform/transform_message.h>
+
+/// PROJECT
+#include <path_msgs/NavigateToGoalAction.h>
+
+/// SYSTEM
+#include <tf/tf.h>
+#include <actionlib/client/simple_action_client.h>
+
+using namespace csapex::connection_types;
+
+namespace csapex
+{
+
+class GoTo : public Node
+{
+public:
+    GoTo()
+        : client_("navigate_to_goal", true),
+          cmd_sent_(false)
+    {
+    }
+
+    void setup(csapex::NodeModifier& modifier) override
+    {
+        event_at_goal_ = modifier.addEvent("at goal");
+        event_error_ = modifier.addEvent("error");
+
+        modifier.addTypedSlot<TransformMessage>("goto", [this](const TokenPtr& token) {
+            if(cmd_sent_) {
+                return;
+            }
+
+            goal_ = std::dynamic_pointer_cast<TransformMessage const>(token->getTokenData());
+            apex_assert(goal_);
+
+            if(!client_.isServerConnected()) {
+                awarn << "waiting for navigation server" << std::endl;
+                client_.waitForServer();
+            }
+
+            path_msgs::NavigateToGoalGoal goal_msg;
+
+            tf::poseTFToMsg(goal_->value, goal_msg.goal.pose.pose);
+            goal_msg.goal.pose.header.frame_id = goal_->frame_id;
+            goal_msg.goal.pose.header.stamp.fromNSec(goal_->stamp_micro_seconds * 1e3);
+            goal_msg.goal.algorithm.data = "patsy_forward";
+
+            goal_msg.failure_mode = path_msgs::NavigateToGoalGoal::FAILURE_MODE_REPLAN;
+            goal_msg.velocity = 0.5;
+
+            ainfo << "sending goal " << goal_msg << std::endl;
+            client_.sendGoal(goal_msg,
+                             boost::bind(&GoTo::goalCallback, this, _1, _2),
+                             boost::bind(&GoTo::activeCallback, this),
+                             boost::bind(&GoTo::feedbackCallback, this, _1));
+
+            cmd_sent_ = true;
+        });
+    }
+
+    void activation()
+    {
+    }
+
+    void deactivation()
+    {
+        cmd_sent_ = false;
+    }
+
+    void goalCallback(const actionlib::SimpleClientGoalState&, const path_msgs::NavigateToGoalResultConstPtr& result)
+    {
+        goal_.reset();
+
+        if(result->status == path_msgs::NavigateToGoalResult::STATUS_SUCCESS) {
+            event_at_goal_->trigger();
+        } else {
+            event_error_->trigger();
+        }
+    }
+
+    void feedbackCallback(const path_msgs::NavigateToGoalFeedbackConstPtr&)
+    {
+
+    }
+
+    void activeCallback()
+    {
+
+    }
+
+    void setupParameters(csapex::Parameterizable& parameters) override
+    {
+    }
+
+    void process()
+    {
+    }
+
+
+
+private:
+    Event* event_at_goal_;
+    Event* event_error_;
+
+    TransformMessage::ConstPtr goal_;
+
+    actionlib::SimpleActionClient<path_msgs::NavigateToGoalAction> client_;
+
+    bool cmd_sent_;
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::GoTo, csapex::Node)
+
+
